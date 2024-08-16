8ca3eac0ed9411d5739cc6f7df48ef80bce26afe
joseph-nashat <39928450+joseph-nashat@users.noreply.github.com>
2024-08-17 01:27:02 +0300

    TPAC-2 initiating the UI
    TPAC-2 initiating the UI

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 #python -m venv env
 #env\scripts\activate
-#pip install langchain langchain-chroma langchain-core langchain-openai langchain-text-splitters langsmith
+#pip install langchain langchain-chroma langchain-core langchain-openai langchain-text-splitters langsmith python-dotenv
